d4ffffe140e8fdd26236603f21e1fc97ce727336
webinga <143020365+webinga@users.noreply.github.com>
2023-08-23 23:40:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const WebSocket = require('ws')
+const APIKEY = process.env.POLY_API_KEY || 'slItgdlk8bR20E2SnvItY_x7b7lXE0SB'
+const ws = new WebSocket('wss://socket.polygon.io/crypto') 
+var msgd = []
+const server = express()
+  .use((req, res) => res.sendFile('/home.html', { root: __dirname }))
+  .listen(process.env.PORT || 3000, () => console.log(`Listening on ${3000}`));
+
+  const { Server } = require('ws');
+
+const ws_server = new Server({ server });
+ws.on('open', () => {
+    console.log('Connected!')
+    ws.send(`{"action":"auth","params":"${APIKEY}"}`)
+    ws.send(`{"action":"subscribe","params":"XT.BTC-USD"}`)
+})
+
+
+ws.on('error', console.log)
+ws_server.on('connection', (ws) => {
+    
+    
+    console.log('New client connected!');
+  
+    ws.on('close', () => console.log('Client has disconnected!'));
+  });
+i=0
+  setInterval(() => {
+    ws.on('message', ( data ) => {
+        data = JSON.parse( data )
+        data.map(( msg ) => {
+            if( msg.ev === 'status' ){
+                return console.log('Status Update:', msg.message)
+            }
+           // console.log(msg)
+            msgd = msg
+            console.log(i++)
+            console.log(typeof msg)
+            console.log(msg.p)
+        })
+    })
+    ws_server.clients.forEach((client) => {
+     // client.send(new Date().toTimeString());
+      client.send(msgd.p);
+    });
+  }, 1000);
